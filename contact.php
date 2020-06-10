94c66b53881530fb660ab1dac98b470faacfe449
ingenious-sides <you@example.com>
2020-06-10 14:34:53 +0200

    MAJ

---
+++ contact.php
@@ -0,0 +1,15 @@
+
+<?php
+include_once("header.php");
+?>
+			<section>
+				Voici le contenu de la page contact<br />
+				Voici le contenu de la page contact<br />
+				Voici le contenu de la page contact<br />
+				Voici le contenu de la page contact<br />
+				Voici le contenu de la page contact<br />
+			</section>
+<?php
+			include_once("footer.php");
+
+?>
